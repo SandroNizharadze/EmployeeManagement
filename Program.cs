33bf751d4c6257f239cf1209a1a02b0ef74a3303
SandroNizharadze <sandronizharadze77@gmail.com>
2025-02-12 17:12:03 +0400

    2

--- Program.cs
+++ Program.cs
@@ -39,5 +39,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
--- obj/Debug/net9.0/EmployeePortal.AssemblyInfo.cs
+++ obj/Debug/net9.0/EmployeePortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeePortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c3efebce23b3cb4fe8a9279aa35530aa90abf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeePortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeePortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
